e161eff7a4ee54c37df5bf7d835cfcfdd9b0d967
Greg Elin <greg@fotonotes.net>
2016-07-17 15:34:20 -0400

    Tweek example text

--- compliancelib/systemcompliance.py
+++ compliancelib/systemcompliance.py
@@ -13,9 +13,12 @@ Visit [tbd] for the latest version.
 Example python CLI
 
 import sys
+import yaml
+import pprint
+
 import compliancelib
-# instantiate an OpenControl object to hold an array of controls
-oc = compliancelib.OpenControl()
+# instantiate an SystemCompliance object to hold an array of controls
+sp = compliancelib.SystemCompliance()
 
 sp.system['name']
 sp.system['name'] = "GovReady WordPress Dashboard"
@@ -61,6 +64,7 @@ sc_system_info = sp.control_details(ck)
 sp.control_details(ck)
 # {'User Account and Authentication (UAA) Server': [{'control_key': 'AC-4', 'standard_key': 'NIST-800-53', 'covered_by': [], 'implementation_status': 'none', 'narrative': [{'text': 'The information system enforces approved authorizations for controlling the flow of information within the system and between interconnected systems based on the 18F Access Control Policy Section 3 -  Information Flow Enforcement which states:\n  - 18F enforces approved authorizations for controlling the flow of information within its information systems and between interconnected systems in accordance with applicable federal laws and 18F policies and procedures.\n  - 18F shall use flow control restrictions to include: keeping export controlled information from being transmitted in the clear to the Internet, blocking outside traffic that claims to be from within the organization and not passing any web requests to the Internet that are not from the internal web proxy.\n  - 18F shall use boundary protection devices (e.g., proxies, gateways, guards, encrypted tunnels, firewalls, and routers) that employ rule sets or establish configuration settings that restrict information system services, provide a packet-filtering capability based on header information, or message-filtering capability based on content (e.g., using key word searches or document characteristics.'}]}]}
 
+
 # Print control info from standard and also systempl implementation details
 print ck
 print sc_standard_info.title
@@ -74,7 +78,8 @@ for compurl in urls:
   sp.system_component_add_from_url(compurl)
 
 pprint.pprint(sp.control_details("AU-1"))
-
+pprint.pprint(sp.control_details("AU-5"))
+pprint.pprint(sp.control_details("SC-10"))
 
 #### OLDER
 
