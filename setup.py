ab38e313e8ebd63ce035e5682c64685597accb70
Greg Elin <greg@fotonotes.net>
2016-10-06 10:36:58 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='compliancelib',
-      version='0.13.2',
+      version='0.13.3',
       description='A python library for modeling IT Compliance',
       long_description=readme(),
       classifiers=[
